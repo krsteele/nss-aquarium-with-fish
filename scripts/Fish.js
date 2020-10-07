b203f21af56ed5abbdf47b3f5d64cacab89d3384
Kristen Steele <ksteelephoto@gmail.com>
2020-10-07 16:29:48 -0500

    add fish component html

---
+++ scripts/Fish.js
@@ -0,0 +1,12 @@
+// renders indiv fish obj as HTML
+export const Fish = (fish) => {
+    return `
+        <section class="fish card">
+            <div><img class="fish__image image--card" src=${fish.image}" /></div>
+            <div class="fish__name">${fish.name}</div>
+            <div class="fish__species">${fish.species}</div>
+            <div class="fish__length">${fish.length}</div>
+            <div class="fish__location">${fish.location}</div>
+            <div class="fish__food">${fish.food}</div>
+        </section>`
+}
