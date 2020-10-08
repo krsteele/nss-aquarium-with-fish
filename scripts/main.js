43089aa67ab1a7a2923cba8738bcf48406908fc9
Kristen Steele <ksteelephoto@gmail.com>
2020-10-08 11:16:13 -0500

    add function to append individual tip code to container html and insert into contentContainer__right and fix import links in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,26 @@
-// imports the useFish function which slices the fishCollection array into indiv fish obj
-import { useFish } from "../fish/FishDataProvider.js"
-import { FishList } from "../fish/FishList.js"
+// imports necessary fish files
+import { useFish } from "./fish/FishDataProvider.js"
+import { FishList } from "./fish/FishList.js"
+
+//imports necessary tips files
+import { useTip } from "./tips/TipDataProvider.js"
+import { TipList } from "./tips/TipList.js"
 
 // calls the useFish array and stores the return value
-const allTheFish = useFish()
+// const allTheFish = useFish()
 
 // for (const fish of allTheFish) {
 //     console.log(fish)
 // }
 
+// test tips data provider
+const allTheTips = useTip()
+for (const tip of allTheTips) {
+    console.log(tip)
+}
+
 // import FishList function so the fish will be rendered immediately when the page loads
 FishList()
+
+// TipList function to render html for tips
+TipList()
